53b73a685a9b22ad639f414780b1a6eb792daf51
dju <jlien7@gmail.com>
2025-02-21 20:12:57 +0100

    configureSupaBase

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,9 +8,9 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        'card': "url('/card.jpg')", 
-        'card1': "url('/card1.jpg')",
-        'card2': "url('/card2.jpg')",
+        'card': "url('/card.webp')", 
+        'card1': "url('/card1.webp')",
+        'card2': "url('/card2.webp')",
         'background': "url('/background.jpg')"
       },
       colors: {
