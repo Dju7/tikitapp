50fe4f3916338e2ce472ba610b4a1e6a74eb7709
dju <jlien7@gmail.com>
2025-02-21 12:27:33 +0100

    modif_login_element

--- app/component/nav/Nav.jsx
+++ app/component/nav/Nav.jsx
@@ -8,20 +8,20 @@ export default function Nav() {
     return (
       <nav className="h-full w-full flex flex-col items-center justify-center gap-1 ">
         <div className="h-1/4 w-full bg-black flex flex-col text-orange-400 justify-center items-center gap-2 hover:bg-white hover:text-black transition-colors duration-300 ease-in-out">
-          <Link href="/board"><FaHome className="text-6xl cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110"/></Link>
-          <p className="text-2xl">ACCUEIL</p>
+          <Link href="/board"><FaHome className="text-5xl lg:text-6xl cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110"/></Link>
+          <p className="text-xl lg:text-2xl">ACCUEIL</p>
         </div>
         <div className="h-1/4 w-full bg-black flex flex-col text-orange-400 justify-center items-center gap-2 hover:bg-white hover:text-black transition-colors duration-300 ease-in-out">
-          <Link href="/board/consignes"><FaGavel className="text-6xl cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110" /></Link>
-          <p className="text-2xl">CONSIGNES</p>
+          <Link href="/board/consignes"><FaGavel className="text-5xl lg:text-6xl cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110" /></Link>
+          <p className="text-xl lg:text-2xl">CONSIGNES</p>
         </div>
         <div className="h-1/4 w-full bg-black flex flex-col text-orange-400 justify-center items-center gap-2 hover:bg-white hover:text-black transition-colors duration-300 ease-in-out">
-        <Link href="/board/anomalies"><FaHouseMedicalFlag className="text-6xl cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110" /></Link>
-          <p className="text-2xl">ANOMALIES</p>
+        <Link href="/board/anomalies"><FaHouseMedicalFlag className="text-5xl lg:text-6xl cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110" /></Link>
+          <p className="text-xl lg:text-2xl">ANOMALIES</p>
         </div>
         <div className="h-1/4 w-full bg-black flex flex-col text-orange-400 justify-center items-center gap-2 hover:bg-white hover:text-black transition-colors duration-300 ease-in-out">
-        <Link href="/board/hs"><FaFireExtinguisher className="text-6xl cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110" /></Link>
-          <p className="text-2xl">HORS SERVICE</p>
+        <Link href="/board/hs"><FaFireExtinguisher className="text-5xl lg:text-6xl cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110" /></Link>
+          <p className="text-xl lg:text-2xl text-center">HORS SERVICE</p>
         </div>
       </nav>
     );
--- app/page.js
+++ app/page.js
@@ -32,13 +32,13 @@ export default function Home() {
   return (
     <main className="relative z-0 h-screen w-full bg-orange-400 flex flex-col justify-center items-center">
       <img src="background.jpg" className="absolute z-1 h-full w-full object-cover opacity-50" />
-      <section className="z-10 w-[30%] h-[60%]  flex flex-col lg:flex-row justify-center items-center overflow-hidden bg-white p-4 rounded-xl">  
+      <section className="z-10 w-[25%] h-[55%]  flex flex-col lg:flex-row justify-center items-center overflow-hidden bg-white/90 p-4 rounded-xl shadow-xl shadow-black/80">  
         <div className="h-[50%] lg:h-full w-full lg:w-[70%] flex flex-col justify-center items-center">
         <img src='913.png' />
-        <h2 className="text-3xl text-neutral-800 font-bold mt-1">PC POMPIER</h2>
+        <h2 className="text-3xl text-orange-600 font-bold mt-1">PC POMPIER</h2>
         <div className="min-h-14 w-full flex justify-center items-center">
         {error && (
-                <p className="w-full bg-red-600 text-white font-bold text-center p-2">{error}</p>
+                <p className="w-full bg-red-600 text-white font-bold text-center p-1 rounded">{error}</p>
               )}
 
         </div>
@@ -46,21 +46,21 @@ export default function Home() {
         className="mt-1 flex flex-col justify-start items-center gap-4 w-full h-[70%] "
         onSubmit={loginUser} method="POST"
         >
-          <label className="text-zinc-600 font-bold">
+          <label className="text-zinc-800 font-bold">
            LOGIN
           </label>
           <input 
           id="name" 
           name="name" type="name"   
           onChange={(e) => { setData({ ...data, name: e.target.value }) }} 
-          className="w-full p-1 border-2 border-black focus:bg-orange-400 text-black font-bold"
+          className="w-full p-1 border-2 border-black focus:bg-orange-400 text-black font-bold rounded"
           />
          
-          <label className="text-zinc-600 font-bold">
+          <label className="text-zinc-800 font-bold">
             PASSWORD
           </label>
           <input 
-          className="w-full p-1 border-2 border-black focus:bg-orange-400 "
+          className="w-full p-1 border-2 border-black focus:bg-orange-400 rounded "
           id="password" 
           name="password" 
           type="password"          
